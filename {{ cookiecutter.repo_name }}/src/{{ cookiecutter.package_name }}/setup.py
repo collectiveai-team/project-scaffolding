14c1ac2b8bd426c1cfb51e8092da7a0fc5553d48
jedzill4 <raulonco@gmail.com>
2023-06-13 12:11:26 -0300

    update devcontainer service

--- {{ cookiecutter.repo_name }}/src/{{ cookiecutter.package_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.package_name }}/setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='{{ cookiecutter.package_name }}',
     packages=find_packages(),
@@ -8,7 +11,7 @@ setup(
     author='{{ cookiecutter.author_name }}',
     author_email='{{ cookiecutter.author_email }}',
     url='{{ cookiecutter.git_repo_url }}',
-    install_requires=[],
+    install_requires=required,
     package_data={'': ['*.yml', '*.yaml']},
     include_package_data=True,
     classifiers=["Programming Language :: Python :: 3"],
